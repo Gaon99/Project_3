c7daa6174997d42a23d7df62d546c08ab8cd92db
sinjid7 <wldmstls01@naver.com>
2025-02-24 17:43:09 +0900

    [chore] DestroyobstacleAdd

--- Assets/02. Scripts/Item/ObstacleItem.cs
+++ Assets/02. Scripts/Item/ObstacleItem.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ObstacleItem : ItemManager
 {
+    public string obstacleTag = "Obstacle";
+    public float destroyRadius = 5f; // 방해물 파괴 범위
+
     public override void DestroyAfterTime()
     {
         Invoke("DestroyObject", 10.0f); // 일정 시간이 지나면 아이템 파괴
@@ -12,4 +15,26 @@ public class ObstacleItem : ItemManager
     {
         Destroy(gameObject);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) //플레이어가 아이템을 먹으면
+        {
+            DestroyObstacles(); // 방해물 파괴
+            Destroy(gameObject); // 먹은 아이템 파괴
+        }
+    }
+    private void DestroyObstacles()
+    {
+        Collider2D[] obstacles = Physics2D.OverlapCircleAll(transform.position, destroyRadius);
+
+        foreach (Collider2D obstacle in obstacles)
+        {
+            if (obstacle.CompareTag(obstacleTag))
+            {
+                Destroy(obstacle.gameObject);  // 방해물 삭제
+            }
+        }
+    }
+
 }
