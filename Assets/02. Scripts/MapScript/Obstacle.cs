68c557aa0cd6227b655811c6481d16463865fbec
Sinneli <nyeliplays1911@gmail.com>
2025-02-25 09:10:32 +0900

    장애물 생성 위치 0.5 값으로 반올림
    좀 저 일정한 간격을 위해 코드 수정

--- Assets/02. Scripts/MapScript/Obstacle.cs
+++ Assets/02. Scripts/MapScript/Obstacle.cs
@@ -5,14 +5,16 @@ using Random = UnityEngine.Random;
 
 public class Obstacle : MonoBehaviour
 {
-    public float highPosY = -1f;
-    public float lowPosY = -2.7f;
+    [SerializeField] private float highPosY = -1f;
+    [SerializeField] private float lowPosY = -2.7f;
 
-    public float minGap = 6f;
-    public float maxGap = 10f;
+    [SerializeField] private float minGap = 6f; // 간격 사이 넓이 최소
+    [SerializeField] private float maxGap = 10f;// 간격 사이 넓이 최소
 
     [SerializeField] private float minDistance = 4f; // 최소 배치
     [SerializeField] private float maxDistance = 8f; // 최대 배치
+
+    [SerializeField] private float roundSize = 0.5f; // 반올림 값. 좀 더 정확히 잘리려고
 
     public Transform topObject;
     public Transform bottomObject;
@@ -20,7 +22,7 @@ public class Obstacle : MonoBehaviour
     public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
     {
         float widthPadding = Random.Range(minDistance, maxDistance);
-        float holeSize = Random.Range(minGap, maxGap) / 2f; //3~5f
+        float holeSize = Mathf.Round(Random.Range(minGap, maxGap)) / 2f; //3~5f
         topObject.localPosition = new Vector3(0, holeSize);
         bottomObject.localPosition = new Vector3(0, -holeSize);
 
