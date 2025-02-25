82db610bb5d667951f44aa2e254677341dac0397
sinjid7 <wldmstls01@naver.com>
2025-02-25 14:47:36 +0900

    [chore] ItemSpawnFix

--- Assets/02. Scripts/Item/ItemSpawn.cs
+++ Assets/02. Scripts/Item/ItemSpawn.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class ItemSpawn : MonoBehaviour
 {
-    public GameObject itemPrefab;
-    public float minX = -6f; //아이템 생성 최소 위치
-    public float maxX = 6f; //아이템 생성 최대 위치
-    public float fixedY = 0f;
-    public float minSpawn = 2f; //아이템 생성 최소 시간
-    public float maxSpawn = 5f; //아이템 생성 최소 시간
+    public GameObject[] itemPrefabs;
+    public float spawnTime = 3f; // 아이템 생성 주기
+    public float spawnDistance = 5f; //아이템 생성 위치를 플레이어에 맞춤
+    public float fixedY = 0; //일단 y값 고정
+    public Transform player;
+
 
     void Start()
     {
-        Debug.Log("아이템을 생성했습니다.");
-        Invoke("SpawnItem", Random.Range(minSpawn, maxSpawn)); // 첫 아이템 생성
+        // Coroutine을 사용하여 주기적으로 아이템 생성
+        StartCoroutine(SpawnItems());
     }
 
-    public void SpawnItem()
+    IEnumerator SpawnItems()
     {
-        float randomX = Random.Range(minX, maxX);
-        Vector2 spawnPosition = new Vector2(randomX, fixedY); // X는 랜덤 Y는 고정
+        while (true)
+        {
+            float spawnX = player.position.x + spawnDistance;
+            Vector2 spawnPosition = new Vector2(spawnX, fixedY);
 
-        Instantiate(itemPrefab, spawnPosition, Quaternion.identity); // 아이템 생성
+            GameObject randomItem = itemPrefabs[Random.Range(0, itemPrefabs.Length)]; // 랜덤하게 아이템프리팹 생성
 
-        Invoke("SpawnItem", Random.Range(minSpawn, maxSpawn));
+            Instantiate(randomItem, spawnPosition, Quaternion.identity); // 아이템 생성
+            Debug.Log("아이템을 생성합니다.");
+
+            yield return new WaitForSeconds(spawnTime);
+        }
     }
+
+    
 }
