6ff33552de1d2cf7708183449600076633acfa3b
Sinneli <nyeliplays1911@gmail.com>
2025-02-26 12:58:47 +0900

    Delete Object
    Obstacle Deletes Objects

--- Assets/02. Scripts/MapScript/Obstacle.cs
+++ Assets/02. Scripts/MapScript/Obstacle.cs
@@ -1,33 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
+
 
 public class Obstacle : MonoBehaviour
 {
-    // [SerializeField] private float highPosY = 2f;
-    // [SerializeField] private float lowPosY = -2f;
-
-    //[SerializeField] private float minGap = 6f; // 간격 사이 넓이 최소
-    //[SerializeField] private float maxGap = 10f;// 간격 사이 넓이 최소
-
-    [SerializeField] private float minDistance = 6f; // 최소 배치
-    [SerializeField] private float maxDistance = 12f; // 최대 배치
-
-    //public Transform topObject;
-    public Transform bottomObject;
-
-    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
+    [SerializeField] private int lifeTime = 10;
+    public void Awake()
     {
-        float widthPadding = Mathf.Round(Random.Range(minDistance, maxDistance));
-        // float holeSize = Mathf.Round(Random.Range(minGap, maxGap)) / 2f; //3~5f, 0.5 간격
-        // topObject.localPosition = new Vector3(0, holeSize);
-        
-
-        Vector3 placePosition = lastPosition + new Vector3(widthPadding, 0);
-        
-        transform.position = placePosition;
-
-        return placePosition;
+        Destroy(gameObject, lifeTime);
     }
+
 }
