397d2cadbe9eb8722cd87cc730c5b1838d5afa16
tmdwp <tmdwp1010@gmail.com>
2025-02-27 15:32:55 +0900

    [fix]  데미지 버그 수정
    장애물과 충돌하지 않았음에도 데미지가 들어오는 상황 수정
    충돌 시 매커니즘 수정
    데미지를 받았을 경우 스프라이트 반투명화 추가
    장애물 프리팹 및 장애물 생성 코드 수정
    GamaManager 불필요한 코드 일부 삭제

--- Assets/02. Scripts/MapScript/ObstacleSpawner.cs
+++ Assets/02. Scripts/MapScript/ObstacleSpawner.cs
@@ -17,7 +17,11 @@ public class ObstacleSpawner : MonoBehaviour
     {
         StartCoroutine(SpawnObstacleGroup());
     }
-
+    enum Ypos
+    {
+        top = 3,
+        bottom = -7
+    }
 
     IEnumerator SpawnObstacleGroup()
     {
@@ -28,6 +32,10 @@ public class ObstacleSpawner : MonoBehaviour
 
             float spawnX = player.position.x + spawnDistance;
             Vector2 spawnP = new Vector2(spawnX, 0);
+            int randY = Random.Range(0, 2);
+            if (randY == 0)
+                spawnP.y = (float)Ypos.bottom;
+            else spawnP.y = (float)Ypos.top;
             GameObject obstacle = objectPrefabs[Random.Range(0, objectPrefabs.Length)];
             Instantiate(obstacle, spawnP, Quaternion.identity);
             
